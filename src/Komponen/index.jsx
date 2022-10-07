6beb7c763781e7e53281a69e765c90dce4b83036
gumelar <gumelar.wicaksono6@gmail.com>
2022-10-08 03:23:34 +0700

    create komponen

---
+++ src/Komponen/index.jsx
@@ -0,0 +1,11 @@
+function test() {
+  return (
+    <div className="test">
+      <div className="nav">
+        <h3>gumelar wicaksono</h3>
+      </div>
+    </div>
+  );
+}
+
+export default test;
