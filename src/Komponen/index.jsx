c7135dcfa381969184974019c2d525289ca65694
gumelar <gumelar.wicaksono6@gmail.com>
2022-10-08 16:04:59 +0700

    creat tes li

--- src/Komponen/index.jsx
+++ src/Komponen/index.jsx
@@ -3,6 +3,12 @@ function test() {
     <div className="test">
       <div className="nav">
         <h3>gumelar wicaksono</h3>
+        <ul>
+          <li>mongodb</li>
+          <li>exspres</li>
+          <li>react</li>
+          <li>nodejs</li>
+        </ul>
       </div>
     </div>
   );
