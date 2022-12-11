622227324ea118698316bce3bd3e719c714ec14f
Gumelarwicaksono <88126249+Gumelarwicaksono@users.noreply.github.com>
2022-12-11 11:13:16 +0700

    Update index.jsx
    update

--- src/Komponen/index.jsx
+++ src/Komponen/index.jsx
@@ -1,12 +1,14 @@
 import ClasKomponen from './partone/ClasKomponen';
 import FungsiKomponen from './partone/FungsiKomponen';
 function test() {
-  const brand = 'gumelar react app';
+  const brand = 'gumelar react app componen';
   const lists = ['mongodb', 'exspres', 'react', 'nodejs'];
   return (
     <div>
       <div>
+        <p>//componen 1// </p>
         <ClasKomponen nama={brand} />
+         <p>//componen 2 // </p>
         <FungsiKomponen nama={lists} />
       </div>
     </div>
